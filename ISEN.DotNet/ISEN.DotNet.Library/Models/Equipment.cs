9bb58aa9a51a5a788cd1e81d9dd6b4b5e242c89c
Xavier MALLAT-DESMORTIERS <xavier.mallat-desmortiers@isen.yncrea.fr>
2017-06-12 11:58:15 +0200

    commit Equipment & Statement

---
+++ ISEN.DotNet/ISEN.DotNet.Library/Models/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ISEN.DotNet.Library.Models
+{
+    public class Equipment : BaseEntity
+    {
+        public int IdOwner { get; set; }
+        public enum Type { SolarPanel, WindTurbine, HydroElectric}
+        public int MaxProduction { get; set; }
+    }
+}
---
+++ ISEN.DotNet/ISEN.DotNet.Library/Models/Statement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ISEN.DotNet.Library.Models
+{
+    public class Statement : Equipment
+    {
+        public DateTime Date { get; set; }
+        public double Production { get; set; }
+    }
+}
